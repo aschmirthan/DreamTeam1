b81b8facf5eec96bfe34021dd980458ac211bbd1
hieudp <33427155+hieudp@users.noreply.github.com>
2017-11-12 15:48:36 +0100

    Add files via upload

--- project/js/reservation.js
+++ project/js/reservation.js
@@ -43,25 +43,30 @@ window.onresize = function(){
 }
 
 //* reservation.js
-
+//Here are the variables for max reservation date
 let maxdate = new Date();
 let maxdd = maxdate.getDate();
 let maxmm = maxdate.getMonth()+1;
 let maxyear = maxdate.getFullYear() + 1;
+//Here are the variables for minimun reservation date (today)
 let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth()+1;
-let yyyy = today.getFullYear();
-if(dd<10){
-    dd='0'+dd
+let toddd = today.getDate();
+let todmm = today.getMonth()+1; //Here we add 1 because we only want max reservation 1 year from today
+let todyear = today.getFullYear();
+//If the date is below 10, a 0 is added before the number
+if(toddd<10){
+    toddd='0'+toddd
     maxdd = "0"+maxdd;
 }
-if(mm<10){
-    mm='0'+mm
+//If the month is below 10, a 0 is added before the number
+if(todmm<10){
+    todmm='0'+todmm
     maxmm = "0"+maxmm
 }
-today = yyyy+'-'+mm+'-'+dd;
+//Put the other variables in the new date
+today = todyear+'-'+todmm+'-'+toddd;
 maxdate = maxyear+'-'+ maxmm+'-'+maxdd;
+//Here we set the attributes min and max, to the today variable and maxdate variable
 document.getElementById("date").setAttribute("min", today);
 document.getElementById("date").setAttribute("max", maxdate);
 
